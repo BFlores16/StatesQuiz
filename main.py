38c07c0b475e9cc135e41cf372298d074ee5092e
Brando <brandoflores16@gmail.com>
2020-12-21 14:51:45 -0700

    Add U.S. map image, CSV file containing states, and GUI

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import turtle
 import pandas
 
+screen = turtle.Screen()
+screen.title("U.S. States Quiz")
+
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+answer_state = screen.textinput(title="State Name", prompt="Enter a state's name.")
